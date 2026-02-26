47f7cf0efe6a7d96f87886fde5a19b79a9e2ecb0
Eliabe G da Paz <eliabepaz.work@gmail.com>
2026-02-26 07:50:52 -0300

    Udpate All files

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,8 +1,4 @@
 <?php
-// app/controllers/AuthController.php
-
-// 🔴 REMOVA ESSA LINHA:
-// session_start();
 
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../models/usuario.php';
